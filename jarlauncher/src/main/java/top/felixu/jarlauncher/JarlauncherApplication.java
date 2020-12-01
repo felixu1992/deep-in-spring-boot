095ba472930804350b9adc3f64d16b469c508897
felixu <xufei_0320@163.com>
2020-12-01 18:00:03 +0800

    :rocket: Spring Boot 之 JarLauncher

---
+++ jarlauncher/src/main/java/top/felixu/jarlauncher/JarlauncherApplication.java
@@ -0,0 +1,13 @@
+package top.felixu.jarlauncher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarlauncherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarlauncherApplication.class, args);
+    }
+
+}
---
+++ jarlauncher/src/test/java/top/felixu/jarlauncher/JarlauncherApplicationTests.java
@@ -0,0 +1,13 @@
+package top.felixu.jarlauncher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarlauncherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
